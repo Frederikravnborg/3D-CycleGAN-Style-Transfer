potential fix to normalization

config.py
@@ -5,7 +5,7 @@ currentTime = currentDateAndTime.strftime("%m.%d.%H.%M.%S")
 
 # Training Loop:
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train_test_100"
+TRAIN_DIR = "data/train_test_10"
 VAL_DIR = "data/train_test_10"
 N_POINTS = 2048
 BATCH_SIZE = 2
@@ -13,6 +13,7 @@ LEARNING_RATE = 1e-4
 DISC_LR_FACTOR = 0.01
 NUM_WORKERS = 0
 MAX_DISTANCE = torch.tensor(1.0428)
+USE_WANDB = False
 
 
 

load_data.py
@@ -5,6 +5,8 @@ import warnings
 import torch
 import trimesh
 import glob
+import numpy as np
+import config
 warnings.filterwarnings("ignore")
 
 # Define a custom dataset class that inherits from Dataset
@@ -13,7 +15,7 @@ class ObjDataset(Dataset):
     def __init__(self, root_female, root_male, transform=None, n_points=2048):
         self.root_female = root_female
         self.root_male = root_male
-        self.transform = transform
+        # self.transform = transform
         self.n_points = n_points
         self.female_pcds = glob.glob(os.path.join(root_female, '*.obj'))
         # self.female_pcds = os.listdir(root_female)
@@ -37,8 +39,14 @@ class ObjDataset(Dataset):
         female_pcd = female_pcd.sample(self.n_points)
         male_pcd = male_pcd.sample(self.n_points)
 
+        # normalize
+        female_pcd = (female_pcd - np.mean(female_pcd, axis=0)) / config.MAX_DISTANCE
+        male_pcd = (male_pcd - np.mean(male_pcd, axis=0)) / config.MAX_DISTANCE
+
         # transform from numpy to tensor
-        female_pcd = torch.from_numpy(female_pcd)
-        male_pcd = torch.from_numpy(male_pcd)
+        # female_pcd = torch.from_numpy(female_pcd)
+        # male_pcd = torch.from_numpy(male_pcd)
 
         return female_pcd, male_pcd
+
+

train.py
@@ -18,6 +18,7 @@ from foldingnet_model import Generator
 import trimesh
 from torchvision import transforms
 import wandb
+import numpy as np
 
 # start a new wandb run to track this script
 wandb.init(
@@ -38,7 +39,8 @@ wandb.init(
     "LOAD_FOLD_MODEL": config.LOAD_FOLD_MODEL,
     "FOLD_SHAPE": config.FOLD_SHAPE,
     "LAMBDA_CYCLE": config.LAMBDA_CYCLE,
-    }
+    },
+    mode = "online" if config.USE_WANDB else "disabled"
 )
 
 def train_fold(gen_M, gen_F, loader, opt_gen, epoch, folder_name):
@@ -226,19 +228,19 @@ def main():
         load_checkpoint(config.CHECKPOINT_DISC_F, disc_F, opt_disc, config.LEARNING_RATE,)
     
     # define transform to normalize point clouds
-    transform = transforms.Lambda(lambda x: x / config.MAX_DISTANCE)
+    transform = transforms.Lambda(lambda x: (x-np.mean(x, axis=0)) / config.MAX_DISTANCE)
     # define train dataset
     dataset = ObjDataset(
         root_male=config.TRAIN_DIR + "/male", 
         root_female=config.TRAIN_DIR + "/female",
-        transform=transform,
+        transform=None,
         n_points=config.N_POINTS
     )
     # define validation dataset
     val_dataset = ObjDataset(
         root_male=config.VAL_DIR + "/male",
         root_female=config.VAL_DIR + "/female",
-        transform=transform,
+        transform=None,
         n_points=config.N_POINTS
     )
 