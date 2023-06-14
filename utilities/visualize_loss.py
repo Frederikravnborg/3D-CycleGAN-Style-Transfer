93b82a3a76e40260ead5725c839174194221aa6a
Lachlan Houston <LachlanHouston111@gmail.com>
2023-06-14 14:16:08 +0200

    baseline pcds

--- utilities/visualize_loss.py
+++ utilities/visualize_loss.py
@@ -1,6 +1,55 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from load_data import ObjDataset
+from torch.utils.data import DataLoader
+import config
+from tqdm import tqdm
+import trimesh
 import numpy as np
+import config
+from sklearn.neighbors import NearestNeighbors
+
+
+def chamfer_distance(x, y, metric='l2', direction='bi'):
+    """Chamfer distance between two point clouds
+    Parameters
+    ----------
+    x: numpy array [n_points_x, n_dims]
+        first point cloud
+    y: numpy array [n_points_y, n_dims]
+        second point cloud
+    metric: string or callable, default ‘l2’
+        metric to use for distance computation. Any metric from scikit-learn or scipy.spatial.distance can be used.
+    direction: str
+        direction of Chamfer distance.
+            'y_to_x':  computes average minimal distance from every point in y to x
+            'x_to_y':  computes average minimal distance from every point in x to y
+            'bi': compute both
+    Returns
+    -------
+    chamfer_dist: float
+        computed bidirectional Chamfer distance:
+            sum_{x_i \in x}{\min_{y_j \in y}{||x_i-y_j||**2}} + sum_{y_j \in y}{\min_{x_i \in x}{||x_i-y_j||**2}}
+    """
+    
+    if direction=='y_to_x':
+        x_nn = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm='kd_tree', metric=metric).fit(x)
+        min_y_to_x = x_nn.kneighbors(y)[0]
+        chamfer_dist = np.mean(min_y_to_x)
+    elif direction=='x_to_y':
+        y_nn = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm='kd_tree', metric=metric).fit(y)
+        min_x_to_y = y_nn.kneighbors(x)[0]
+        chamfer_dist = np.mean(min_x_to_y)
+    elif direction=='bi':
+        x_nn = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm='kd_tree', metric=metric).fit(x)
+        min_y_to_x = x_nn.kneighbors(y)[0]
+        y_nn = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm='kd_tree', metric=metric).fit(y)
+        min_x_to_y = y_nn.kneighbors(x)[0]
+        chamfer_dist = np.mean(min_y_to_x) + np.mean(min_x_to_y)
+    else:
+        raise ValueError("Invalid direction type. Supported types: \'y_x\', \'x_y\', \'bi\'")
+        
+    return chamfer_dist
 
 def visualize_loss():
     filename = "output/loss_06.07.15.18.59.csv"
@@ -37,6 +86,50 @@ def visualize_loss():
     ax2.set(xlabel='Epoch', ylabel='Loss', title='discriminator loss')
     plt.show()
 
+# Function that takes Chamfer Loss for all combinations of generated models, and real models
+def visualize_chamfer_loss(val_loader, gen_loader):
+    meshes = []
+    gen_meshes = []
 
-if __name__ == '__main__':
-    visualize_loss()
+    loop = tqdm(val_loader, leave=True) #Progress bar
+    gen_loop = tqdm(gen_loader, leave=True) #Progress bar
+
+    for _, (female, male) in enumerate(loop):
+        female = female.to(config.DEVICE)
+        male = male.to(config.DEVICE)
+
+        meshes.append(female)
+
+    for _, (gen_female, gen_male) in enumerate(gen_loop):
+        gen_female = gen_female.to(config.DEVICE)
+        gen_male = gen_male.to(config.DEVICE)
+
+        gen_meshes.append(gen_female)
+
+
+    
+
+    # loop through the data loader
+    
+    
+    return meshes, gen_meshes
+
+
+# load meshes data
+losses = np.load("losses.npy")
+
+# visualize loss in histogram
+plt.hist(np.mean(losses, axis=1), bins=100)
+plt.xlabel('Chamfer distance')
+plt.ylabel('Frequency')
+plt.title('Chamfer distance between generated meshes')
+plt.show()
+
+losses = np.load("gen_losses.npy")
+
+# visualize loss in histogram
+plt.hist(np.mean(losses, axis=1), bins=100)
+plt.xlabel('Chamfer distance')
+plt.ylabel('Frequency')
+plt.title('Chamfer distance between generated meshes')
+plt.show()
